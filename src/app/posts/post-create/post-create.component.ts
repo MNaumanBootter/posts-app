670ae312f0e692f16cc3501c32106aaed58f6992
Muhammad Nauman Bootter <mnaumanbootter@gmail.com>
2021-08-25 19:28:06 +0500

    added post create component with textarea for input and paragraph for output with one way binding

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-post-create',
+	templateUrl: './post-create.component.html',
+	styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  newPost = 'NO CONTENT';
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+	onAddPost(postInput: HTMLTextAreaElement) {
+
+    this.newPost = postInput.value;
+
+	}
+
+}
